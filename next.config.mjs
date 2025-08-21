708784cea6f004e6d23404464068db1f177a762e
haonian <longmosheng@gmail.com>
2025-08-21 13:18:48 +0800

    Configure for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/eduvis-site' : '',
-  assetPrefix: isProd ? '/eduvis-site/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
